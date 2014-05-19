6b7ca7380e1ae141894034cb8e3000cd4078ab83
Roberto Ferranti <rf@emaze.net>
2014-05-19 11:38:08 +0200

    fix: wrong test

--- src/test/java/net/emaze/dysfunctional/jackson/ranges/RangeTest.java
+++ src/test/java/net/emaze/dysfunctional/jackson/ranges/RangeTest.java
@@ -1,6 +1,5 @@
 package net.emaze.dysfunctional.jackson.ranges;
 
-import net.emaze.dysfunctional.jackson.ranges.RangeModule;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,13 +19,24 @@ public class RangeTest {
     private static final Ranges<Long> LONG_RANGES = new Ranges<Long>(new ComparableComparator<Long>(), new NextLongSequencingPolicy(), 0L);
 
     @Test
+    public void canSerializeAndDeserialize() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new DysfunctionalModule());
+        mapper.registerModule(new RangeModule<Integer>(RANGES, Integer.class));
+        
+        final Range<Integer> sourceRange = RANGES.closed(1, 3);
+        final RangeBean source = RangeBean.of(sourceRange);
+        final RangeBean got = mapper.readValue(mapper.writeValueAsString(source), RangeBean.class);
+        Assert.assertEquals(sourceRange, got.getInner());
+    }
+    
+    @Test
     public void canDeserialize() throws IOException {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new DysfunctionalModule());
         mapper.registerModule(new RangeModule<Integer>(RANGES, Integer.class));
-        final RangeBean bean = mapper.readValue("{'inner':[{'b':1,'e':[2]}]}".replace('\'', '"'), RangeBean.class);
-        final Range<Integer> range = RANGES.closed(1, 2);
-        Assert.assertEquals(range, bean.getInner());
+        final RangeBean bean = mapper.readValue("{'inner':[{'b':1,'e':[3]}]}".replace('\'', '"'), RangeBean.class);
+        Assert.assertEquals(RANGES.closed(1, 2), bean.getInner());
     }
 
     @Test(expected = JsonMappingException.class)
